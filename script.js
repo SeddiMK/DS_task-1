cb6d4fda1d04a6d938439f6165cec73ea72c30e0
SeddiMK <web.egorovm@gmail.com>
2024-10-16 11:53:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let lastScrollTop = 0
+const header = document.querySelector('.header')
+
+// window.addEventListener('scroll', function () {
+// 	const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+// 	if (scrollTop > lastScrollTop) {
+// 		header.style.top = '-100px' // Hide header
+// 	} else {
+// 		header.style.top = '0' // Show header
+// 	}
+// 	lastScrollTop = scrollTop
+// })
